fe0e797fc8f81671f4dfdeca7f32f59cb2b4dbb8
muhamsh <muhammad-ashraful@outlook.com>
2025-09-28 03:33:57 +0600

    prisma edge config

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@/components/ui/button";
 
 export default async function Home ()
 {
-  // const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany();
 
-  // console.log( users );
+  console.log( users );
 
   return (
     <div>
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,9 @@
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { PrismaClient } from "../generated/prisma/";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+    globalForPrisma.prisma || new PrismaClient().$extends( withAccelerate() );
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
